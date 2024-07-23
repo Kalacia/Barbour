2c7fbb50ea51032334b26b19deb98ced8736415a
Kalacia <kalacia@hotmail.co.uk>
2024-07-23 13:25:08 +0100

    created user repo, added moqs for tests, and test can recall a user from the moq repo

---
+++ Library/UnitTests/TestsBase.cs
@@ -0,0 +1,25 @@
+﻿using Library.Models;
+using Library.Repositories;
+using Moq;
+
+namespace UnitTests
+{
+    public class TestsBase
+    {
+        protected readonly Mock<IServiceProvider> MockServiceProvider;
+        protected readonly Mock<IUserRepository> MockServiceUserRepository;
+
+        protected TestsBase()
+        {
+            MockServiceProvider = new Mock<IServiceProvider>();
+            MockServiceUserRepository = new Mock<IUserRepository>();
+        }
+
+        protected void SetupSuccessfulUserMocks(List<UserViewModel> users)
+        {
+            MockServiceProvider.Setup(x => x.GetService(typeof(IUserRepository))).Returns(MockServiceUserRepository.Object);
+
+            MockServiceUserRepository.Setup(x => x.GetAllUsers()).Returns(users);
+        }
+    }
+}
