124f8d7759f02a45ce0a1dbeffe46d968704546c
Kalacia <kalacia@hotmail.co.uk>
2024-07-23 13:25:08 +0100

    created user repo, added moqs for tests, and test can recall a user from the moq repo

--- Library/Library/Models/UserViewModel.cs
+++ Library/Library/Models/UserViewModel.cs
@@ -4,7 +4,13 @@
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
-        public string Email { get; set; }
         public bool Active {  get; set; }
+
+        public UserViewModel(string name, bool active) 
+        {
+            Id = new Guid();
+            Name = name;
+            Active = active;
+        }
     }
 }
