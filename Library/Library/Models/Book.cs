ef19131111f52cc44617daf9c9f6f2f8d302582e
Kalacia <kalacia@hotmail.co.uk>
2024-07-23 15:12:59 +0100

    initial repo, test and book classes made

---
+++ Library/Library/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace Library.Models
+{
+    public class Book
+    {
+        public int ISBN { get; set; }
+        public string Title { get; set; }
+        public string Author{ get; set; }
+        public bool AvailabilityStatus { get; set; }
+        public UserViewModel CheckedOutBy { get; set; }
+
+        public List<History> History { get; set; }
+    }
+}
---
+++ Library/Library/Repositories/IBookRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Library.Repositories
+{
+    public interface IBookRepository
+    {
+    }
+}
