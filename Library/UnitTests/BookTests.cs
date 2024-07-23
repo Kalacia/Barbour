fb285d56d021e8d5be652d98fb8f3237c5b1d0ee
Kalacia <kalacia@hotmail.co.uk>
2024-07-23 16:41:57 +0100

    Started book tests

--- Library/UnitTests/BookTests.cs
+++ Library/UnitTests/BookTests.cs
@@ -1,7 +1,21 @@
-﻿namespace UnitTests
+﻿using Library.Models;
+
+namespace UnitTests
 {
     public class BookTests
     {
+
+        [Fact]
+        public void BookShouldBeRetreiveableFromRepo()
+        {
+            //arrange
+            var book = new Book();
+
+            //act
+
+
+            //assert
+        }
 
 
     }
